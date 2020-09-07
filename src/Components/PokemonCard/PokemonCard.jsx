3186f34eed7bb81717725fe478aa421c2011fb08
Shams Ibne Noor <shams.rupom@gmail.com>
2020-09-07 23:56:37 +0600

    adding basic card design

--- src/Components/PokemonCard/PokemonCard.jsx
+++ src/Components/PokemonCard/PokemonCard.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './pokemoncard.css';
 import defaultType from '../../resources/images/grass.png';
-import defaultPokemon from '../../resources/images/defaultpok.jpg';
+import defaultPokemon from '../../resources/images/unnamed.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWeightHanging, faDumbbell, faSun, faRuler } from '@fortawesome/free-solid-svg-icons';
 
 export default function PokemonCard({ pokemon }) {
   return (
@@ -15,10 +17,28 @@ export default function PokemonCard({ pokemon }) {
         </div>
       </div>
       <div className="card-avatar">
-        <img style={{ borderRadius: "100px" }} height="175" width="175" src={defaultPokemon} />
+        <img style={{ borderRadius: "100px" }} height="160" width="175" src={defaultPokemon} />
       </div>
-      <p>{pokemon.avatar}</p>
-      <p>{pokemon.hp}</p>
+      <div className="card-details">
+        <div className="details-top">
+          <div className="single-stat">
+            <FontAwesomeIcon icon={faSun} />&nbsp;
+            <p>70</p>
+          </div>
+          <div className="single-stat">
+            <FontAwesomeIcon icon={faRuler} />&nbsp;
+            <p>40</p>
+          </div>
+          <div className="single-stat">
+            <FontAwesomeIcon icon={faWeightHanging} />&nbsp;
+            <p>400</p>
+          </div>
+        </div>
+        <div className="details-bottom">
+          <p>{pokemon.avatar}</p>
+          <p>{pokemon.hp}</p>
+        </div>
+      </div>
     </div>
   )
 }
